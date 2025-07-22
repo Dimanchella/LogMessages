cd633159f171b2d1d055b39467400cc320cca91d
Dimanchella <dimuka0901@gmail.com>
2025-07-22 19:07:00 +0300

    Delete unused import

--- src/main/java/ru/dim/service/LogMessagesService.java
+++ src/main/java/ru/dim/service/LogMessagesService.java
@@ -10,7 +10,6 @@ import ru.dim.provider.LogMessagesProvider;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
